56003abe6641b44eaa62188e20bdd64e6dd6eb56
Thao Le <lpthao150504@gmail.com>
2025-01-04 08:38:14 +0700

    fix login + signup

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -4,11 +4,14 @@ import TodayPage from '../pages/TodayPage/TodayPage';
 import UpcomingDayPage from '../pages/UpcomingPage/UpcomingDayPage';
 import UpcomingMonthPage from '../pages/UpcomingPage/UpcomingMonthPage';
 import Login from '../pages/Login/Login';
+// import HomePage from '../pages/HomePage/HomePage'
+
 export const publicRoutes = [
     { path: '/', component: <TakeNote /> },
     { path: '/signup', component: <SignUp /> },
     { path: '/today', component: <TodayPage /> },
     { path: '/upcoming-day', component: <UpcomingDayPage /> },
     { path: '/upcoming', component: <UpcomingMonthPage /> },
-    { path: '/Login', component: <Login/>}
+    { path: '/login', component: <Login/>}
+    // { path: '/home', component: <HomePage/>}
 ];
